bcff212c4aab017b98d309e8f85143687bdfe57c
Muhammad Hafiy Dini <81870804+Hafiy29@users.noreply.github.com>
2021-04-16 16:44:51 +0800

    Add files via upload

---
+++ useful_tools.py
@@ -0,0 +1,13 @@
+# file created for Modules and Pip file
+import random
+
+feet_in_mile = 5280
+meters_in_kilometers = 1000
+beatles =["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"]
+
+
+def get_file_ext(filename):
+    return filename[filename.index(".") +1:]
+
+def roll_dice(num):
+    return random.randint(1, num)
