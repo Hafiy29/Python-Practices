bffd77a36b1a0a5684721d3202fe5bbab73a67f6
Muhammad Hafiy Dini <81870804+Hafiy29@users.noreply.github.com>
2021-04-18 17:17:32 +0800

    Add files via upload

---
+++ Inheritence.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+from MalayChef import MalayChef
+
+myChef = Chef()
+myChef.make_chicken()
+myChef.make_special_dish()
+
+myMalayChef = MalayChef()
+myMalayChef.make_chicken()
---
+++ MalayChef.py
@@ -0,0 +1,6 @@
+from Chef import Chef
+
+class MalayChef(Chef):
+
+    def makes_fried_rice(self):
+        print("The chef makes fried rice")
